dfdf5d2be518b9075260ebb66da10a001b7e553f
Kimsg130 <csjh0@naver.com>
2023-03-23 21:10:14 +0900

    version 0.0.1 release(23-03-23) @Done : - Create a project and build - Commited first on GitHub @TODO : - Studying react and springboot - Think to project Structure

---
+++ gyu-stagram/src/main/java/com/kimsg130/gyustagram/GyuStagramApplication.java
@@ -0,0 +1,13 @@
+package com.kimsg130.gyustagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyuStagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GyuStagramApplication.class, args);
+	}
+
+}
---
+++ gyu-stagram/src/test/java/com/kimsg130/gyustagram/GyuStagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimsg130.gyustagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyuStagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
