7270c64fdb7b0799a4c987c2f98ee3bd5ccbd1ab
Kimsg130 <csjh0@naver.com>
2023-03-24 00:51:28 +0900

    version 0.0.1.5 release(23-03-24) @Done : - Studying springboot @TODO : - Studying react - Think to project Structure - Connect DB - Make LoginPage

---
+++ src/main/java/com/kimsg130/gyustagram/controller/ExamController.java
@@ -0,0 +1,14 @@
+package com.kimsg130.gyustagram.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ExamController {
+    @GetMapping("welcome")
+    public String welcome(Model model){
+        model.addAttribute("data", "hello!!!!!!!!");
+        return "welcome";
+    }
+}
