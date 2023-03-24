589fb2eb38cb64a8fdaebaf38f6c67b5b8f7f3ec
Kimsg130 <csjh0@naver.com>
2023-03-24 21:01:33 +0900

    version 0.0.2 release(23-03-24) @Done : - Connect back, frontend port @TODO : - Studying react - Think to project Structure - Connect DB - Make LoginPage

---
+++ src/main/java/com/kimsg130/gyustagram/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.kimsg130.gyustagram.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/api/hello")
+    public String test() {
+        return "Hello, world!";
+    }
+}
